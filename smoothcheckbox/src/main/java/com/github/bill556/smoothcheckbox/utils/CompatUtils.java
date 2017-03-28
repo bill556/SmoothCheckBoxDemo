5047b7772f87a1e0e7a15e54cfce4fc1837745b0
Bill <411372896@qq.com>
2017-03-28 16:33:36 +0800

    Initial commit

---
+++ smoothcheckbox/src/main/java/com/github/bill556/smoothcheckbox/utils/CompatUtils.java
@@ -0,0 +1,13 @@
+package com.github.bill556.smoothcheckbox.utils;
+
+import android.content.Context;
+
+/**
+ * 描述 : 工具类
+ */
+public class CompatUtils {
+    public static int dp2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+}
